7c732ce3a716ea3975bceca5e4c0182162076d75
astridzhu <astridzhu8@gmail.com>
2021-06-29 19:31:04 +1000

    updated url

--- bbquote/lib.py
+++ bbquote/lib.py
@@ -3,11 +3,11 @@ import requests
 
 
 def get_quote():
-    # url = 'https://movie-quote-api.herokuapp.com/v1/quote/'  # alternative API
-    url = 'https://breaking-bad-quotes.herokuapp.com/v1/quotes'
-    response = requests.get(url).json()[0]
+    url = 'https://movie-quote-api.herokuapp.com/v1/quote/'  # alternative API
+    # url = 'https://breaking-bad-quotes.herokuapp.com/v1/quotes'
+    response = requests.get(url).json()
 
-    return f"{response['quote']}    - {response['author']}"
+    return f"{response['quote']}    - {response['role']}"
 
 
 if __name__ == "__main__":
